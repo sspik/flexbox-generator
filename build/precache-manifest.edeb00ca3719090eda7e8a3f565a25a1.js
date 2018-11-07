7fbb343fe7c62a8d99a2797a2f9f442fc1784dc9
Oleg <sspik@mail.ru>
2018-11-07 18:26:55 +0300

    Final.. i think

---
+++ build/precache-manifest.edeb00ca3719090eda7e8a3f565a25a1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "65579e4c881a3b038344",
+    "url": "/static/js/main.65579e4c.chunk.js"
+  },
+  {
+    "revision": "22d53937721b7736903d",
+    "url": "/static/js/1.22d53937.chunk.js"
+  },
+  {
+    "revision": "65579e4c881a3b038344",
+    "url": "/static/css/main.ae510ed7.chunk.css"
+  },
+  {
+    "revision": "d7676eb3228349adb9f89764537ceca0",
+    "url": "/index.html"
+  }
+];
