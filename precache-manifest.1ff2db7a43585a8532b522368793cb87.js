d198c1bb4c54198862167dd94eb7dc1908c768b3
spik <sspik@mail.ru>
2019-02-12 14:47:15 +0300

    init

---
+++ precache-manifest.1ff2db7a43585a8532b522368793cb87.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1093e8c7f9bf29f8d820",
+    "url": "/static/js/main.1093e8c7.chunk.js"
+  },
+  {
+    "revision": "22d53937721b7736903d",
+    "url": "/static/js/1.22d53937.chunk.js"
+  },
+  {
+    "revision": "1093e8c7f9bf29f8d820",
+    "url": "/static/css/main.e126d97d.chunk.css"
+  },
+  {
+    "revision": "c63f0dbe13b33a752e5d2fdb13f8aca5",
+    "url": "/index.html"
+  }
+];
